078a6a59d3601e2fc443dc5c32583176ddda4be8
thetriplej <ssing81@nate.com>
2018-06-17 12:31:13 +0900

    수정사항 수정

--- application/models/Board_model.php
+++ application/models/Board_model.php
@@ -417,7 +417,8 @@ class Board_model extends CI_Model {
 
 
     public function movie_img(){
-        $query = "select f_Index,b_index,f_name ,f_rename,file_path,reg_date from boardfile ";
+        //$query = "select f_Index,b_index,f_name ,f_rename,file_path,reg_date from boardfile ";
+        $query ="select bf.f_Index,bf.b_index,bf.f_name ,bf.f_rename,bf.file_path,bf.reg_date from board as b left join boardfile bf on b.b_index = bf.b_index where bf.f_index is not null";
 
         return $this->db->query($query)->result();
     }
