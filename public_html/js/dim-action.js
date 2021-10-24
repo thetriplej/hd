231a7e7cd2e6bc2cc6d9a5376ba5bfcae78e9a59
thetriplej <ssing81@nate.com>
2021-10-24 23:55:38 +0900

    dev hassed work

---
+++ public_html/js/dim-action.js
@@ -0,0 +1,175 @@
+
+(function($){
+$(document).ready(function () {
+
+	$("#btn_A").click(function () {
+            $("#div_A").css({top: 0});
+			$("#btn_A").css("background-color","#454545");
+			$("#btn_A").css("color","#ffffff");
+			$("#btn_B").css("background-color","#ffffff");
+			$("#btn_B").css("color","#454545");
+			$("#btn_C").css("background-color","#ffffff");
+			$("#btn_C").css("color","#454545");
+			$("#btn_D").css("background-color","#ffffff");
+			$("#btn_D").css("color","#454545");
+			$("#btn_E").css("background-color","#ffffff");
+			$("#btn_E").css("color","#454545");
+        }
+    );
+
+	$("#btn_B").click(function () {
+            $("#div_A").css({top: -450});
+			$("#btn_A").css("background-color","#ffffff");
+			$("#btn_A").css("color","#454545");
+			$("#btn_B").css("background-color","#454545");
+			$("#btn_B").css("color","#ffffff");
+			$("#btn_C").css("background-color","#ffffff");
+			$("#btn_C").css("color","#454545");
+			$("#btn_D").css("background-color","#ffffff");
+			$("#btn_D").css("color","#454545");
+			$("#btn_E").css("background-color","#ffffff");
+			$("#btn_E").css("color","#454545");
+        }
+    );
+
+	$("#btn_C").click(function () {
+            $("#div_A").css({top: -900});
+			$("#btn_A").css("background-color","#ffffff");
+			$("#btn_A").css("color","#454545");
+			$("#btn_B").css("background-color","#ffffff");
+			$("#btn_B").css("color","#454545");
+			$("#btn_C").css("background-color","#454545");
+			$("#btn_C").css("color","#ffffff");
+			$("#btn_D").css("background-color","#ffffff");
+			$("#btn_D").css("color","#454545");
+			$("#btn_E").css("background-color","#ffffff");
+			$("#btn_E").css("color","#454545");
+        }
+    );
+
+	$("#btn_D").click(function () {
+            $("#div_A").css({top: -1350});
+			$("#btn_A").css("background-color","#ffffff");
+			$("#btn_A").css("color","#454545");
+			$("#btn_B").css("background-color","#ffffff");
+			$("#btn_B").css("color","#454545");
+			$("#btn_C").css("background-color","#ffffff");
+			$("#btn_C").css("color","#454545");
+			$("#btn_D").css("background-color","#454545");
+			$("#btn_D").css("color","#ffffff");
+			$("#btn_E").css("background-color","#ffffff");
+			$("#btn_E").css("color","#454545");
+        }
+    );
+
+	$("#btn_E").click(function () {
+            $("#div_A").css({top: -1800});
+			$("#btn_A").css("background-color","#ffffff");
+			$("#btn_A").css("color","#454545");
+			$("#btn_B").css("background-color","#ffffff");
+			$("#btn_B").css("color","#454545");
+			$("#btn_C").css("background-color","#ffffff");
+			$("#btn_C").css("color","#454545");
+			$("#btn_D").css("background-color","#ffffff");
+			$("#btn_D").css("color","#454545");
+			$("#btn_E").css("background-color","#454545");
+			$("#btn_E").css("color","#ffffff");
+        }
+    );
+
+	// 전체메뉴
+    $("#btn_coral").click(function () {
+            $("#div_A").css({left: 0});
+        }
+    );
+	$("#btn_southbeach").click(function () {
+            $("#div_A").css({left: -1000});
+        }
+    );
+	$("#btn_red").click(function () {
+            $("#div_A").css({left: -2000});
+        }
+    );
+	$("#btn_yellow").click(function () {
+            $("#div_A").css({left: -3000});
+        }
+    );
+	$("#btn_green").click(function () {
+            $("#div_A").css({left: -4000});
+        }
+    );
+	$("#btn_sky").click(function () {
+            $("#div_A").css({left: -5000});
+        }
+    );
+	$("#btn_sweetviolet").click(function () {
+            $("#div_A").css({left: -6000});
+        }
+    );
+	$("#btn_darkblue").click(function () {
+            $("#div_A").css({left: -7000});
+        }
+    );
+	$("#btn_darkgreen").click(function () {
+            $("#div_A").css({left: -8000});
+        }
+    );
+	$("#btn_gray").click(function () {
+            $("#div_A").css({left: -9000});
+        }
+    );
+	$("#btn_purple").click(function () {
+            $("#div_A").css({left: -10000});
+        }
+    );
+	$("#btn_sandygray").click(function () {
+            $("#div_A").css({left: -11000});
+        }
+    );
+	$("#btn_landbrown").click(function () {
+            $("#div_A").css({left: -12000});
+        }
+    );
+	$("#btn_burgundy").click(function () {
+            $("#div_A").css({left: -13000});
+        }
+    );
+	$("#btn_camal").click(function () {
+            $("#div_A").css({left: -14000});
+        }
+    );
+	$("#btn_lightpink").click(function () {
+            $("#div_A").css({left: -15000});
+        }
+    );
+	$("#btn_neomint").click(function () {
+            $("#div_A").css({left: -16000});
+        }
+    );
+	$("#btn_goldlime").click(function () {
+            $("#div_A").css({left: -17000});
+        }
+    );
+	$("#btn_deeppeach").click(function () {
+            $("#div_A").css({left: -18000});
+        }
+    );
+	$("#btn_bettablue").click(function () {
+            $("#div_A").css({left: -19000});
+        }
+    );
+	$("#btn_blue").click(function () {
+            $("#div_A").css({left: -20000});
+        }
+    );
+	$("#btn_beige").click(function () {
+            $("#div_A").css({left: -21000});
+        }
+    );
+
+
+
+});
+
+
+})(jQuery);
---
+++ public_html/js/main-action.js
@@ -0,0 +1,104 @@
+(function($){
+$(document).ready(function () {
+
+
+//////////////////////////////// 팝업 사라지기 ////////
+
+		//   $('#hclose').click(function(){
+		//	   $('#hpopup').hide(100);
+		//   });
+
+
+
+
+
+
+/////////////2초마다 실행되는 함수 /////
+
+
+setInterval(function() {
+ var bodyWidth = 70;
+  var bodyHeight = 80;
+  var randPosX = Math.floor((Math.random()*bodyWidth));
+  var randPosY = Math.floor((Math.random()*bodyHeight));
+  //var posLog = document.getElementById('pos_log');
+  //var posXY = 'x: ' + randPosX + '<br />' + 'y: ' + randPosY;
+  $('#rand_pos').animate({'left':randPosX-70},800).animate({'top':randPosY-80},800);
+  }, 1000);
+});
+
+
+
+
+
+$('.rect').on({
+ mouseenter: function(){
+ var bodyWidth = 70;
+  var bodyHeight = 80;
+  var randPosX = Math.floor((Math.random()*bodyWidth));
+  var randPosY = Math.floor((Math.random()*bodyHeight));
+  //var posLog = document.getElementById('pos_log');
+  //var posXY = 'x: ' + randPosX + '<br />' + 'y: ' + randPosY;
+  $('#rand_pos').animate({'left':randPosX-70},800).animate({'top':randPosY-80},800);
+  }
+});
+
+$('.rect').on({
+ mouseleave: function(){
+ var bodyWidth = 70;
+  var bodyHeight = 80;
+  var randPosX = Math.floor((Math.random()*bodyWidth));
+  var randPosY = Math.floor((Math.random()*bodyHeight));
+  //var posLog = document.getElementById('pos_log');
+  //var posXY = 'x: ' + randPosX + '<br />' + 'y: ' + randPosY;
+  $('#rand_pos').animate({'left':randPosX-70},{'top':randPosY-80},800);
+  //$('#rand_pos').animate({'top':randPosY-80},800); 
+  }
+});
+
+
+ $("#toraybook").on({
+ mouseenter: function () {
+  var bodyWidth = 70;
+  var bodyHeight = 80;
+  var randPosX = Math.floor((Math.random()*bodyWidth));
+  var randPosY = Math.floor((Math.random()*bodyHeight));
+  var posLog = document.getElementById('pos_log');
+  var posXY = 'x: ' + randPosX + '<br />' + 'y: ' + randPosY;
+  $('#rand_pos').animate({'left':randPosX-70},800);
+  $('#rand_pos').animate({'top':randPosY-80},800); 
+  posLog.innerHTML = posXY
+ }
+});
+
+
+$('.rect').everyTime(2000, function(){
+				$(this).animate({'opacity':0},800).animate({'opacity':1},800);
+			});
+
+
+$('.new_pos').click(function() {
+
+  var bodyWidth = document.body.clientWidth;
+  var bodyHeight = document.body.clientHeight;
+  var randPosX = Math.floor((Math.random()*bodyWidth));
+  var randPosY = Math.floor((Math.random()*bodyHeight));
+  var posLog = document.getElementById('pos_log');
+  var posXY = 'x: ' + randPosX + '<br />' + 'y: ' + randPosY;
+  $('#rand_pos').css('left', randPosX);
+  $('#rand_pos').css('top', randPosY); 
+  posLog.innerHTML = posXY
+
+});
+
+
+
+
+});
+
+
+
+
+})(jQuery);
+
+
