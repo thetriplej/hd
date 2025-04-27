aee8c49c487d2ca8272848e5ce8b57a45fcdb760
thetriplej <ssing81@nate.com>
2025-04-27 19:58:17 +0900

    팝업위치조정

--- application/config/constants.php
+++ application/config/constants.php
@@ -95,6 +95,6 @@ define('CSS_MOBILE_DIR', '/public_html/m/css');
 define('IMG_MOBILE_DIR', '/public_html/m/images');
 define('JS_MOBILE_DIR', '/public_html/m/js');
 
-define('CACHE_VERSION', '20250409');
+define('CACHE_VERSION', '20250410');
 
 
